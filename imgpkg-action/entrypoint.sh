255a6fc868b28166b43bd2f701ab91bda7243384
Sridhar Vennela <svennela@vmware.com>
2021-05-17 11:09:10 -0700

    carvel image copy

--- imgpkg-action/entrypoint.sh
+++ imgpkg-action/entrypoint.sh
@@ -8,9 +8,9 @@ docker_version=20.10.6
 wget $docker_url/docker-$docker_version.tgz
 tar zxvf docker-20.10.6.tgz --strip 1 -C /usr/bin docker/docker
 imgpkg copy -i ${INPUT_SOURCE_REGISTRY} --to-tar=${INPUT_SOURCE_REGISTRY}.tar
-echo "${{ INPUT_DESTINATION_REGISTRY_USERNAME }}" | docker login "${{ INPUT_DESTINATION_REGISTRY }}" -u "${{ INPUT_DESTINATION_REGISTRY_USERNAME }}" --password-stdin
-imgpkg copy --tar nginx-1.20-alpine.tar --to-repo "${{ INPUT_DESTINATION_REGISTRY }}"/test-repo/nginx
-sh -c "$INPUT_SOURCE_REGISTRY"
+#echo "${{ INPUT_DESTINATION_REGISTRY_USERNAME }}" | docker login "${{ INPUT_DESTINATION_REGISTRY }}" -u "${{ INPUT_DESTINATION_REGISTRY_USERNAME }}" --password-stdin
+#imgpkg copy --tar nginx-1.20-alpine.tar --to-repo "${{ INPUT_DESTINATION_REGISTRY }}"/test-repo/nginx
+#sh -c "$INPUT_SOURCE_REGISTRY"
 echo "----------------------------"
 echo $INPUT_SOURCE_REGISTRY
 echo $INPUT_DESTINATION_REGISTRY
