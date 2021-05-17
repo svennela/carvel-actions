f6b9a6ffa83d7a5e68dbeec68777cb32101f1496
Sridhar Vennela <svennela@vmware.com>
2021-05-17 11:03:36 -0700

    carvel image copy

--- imgpkg-action/entrypoint.sh
+++ imgpkg-action/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -l
 
+wget https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/v0.7.0/imgpkg-linux-amd64
+sudo chmod +x imgpkg-linux-amd64
+sudo mv imgpkg-linux-amd64 /usr/bin/imgpkg
+docker_url=https://download.docker.com/linux/static/stable/x86_64
+docker_version=20.10.6
+wget $docker_url/docker-$docker_version.tgz
+sudo tar zxvf docker-20.10.6.tgz --strip 1 -C /usr/bin docker/docker
+imgpkg copy -i ${INPUT_SOURCE_REGISTRY} --to-tar=${INPUT_SOURCE_REGISTRY}.tar
+echo "${{ INPUT_DESTINATION_REGISTRY_USERNAME }}" | docker login "${{ INPUT_DESTINATION_REGISTRY }}" -u "${{ INPUT_DESTINATION_REGISTRY_USERNAME }}" --password-stdin
+imgpkg copy --tar nginx-1.20-alpine.tar --to-repo "${{ INPUT_DESTINATION_REGISTRY }}"/test-repo/nginx
 sh -c "$INPUT_SOURCE_REGISTRY"
 echo "----------------------------"
 echo $INPUT_SOURCE_REGISTRY
@@ -7,4 +17,3 @@ echo $INPUT_DESTINATION_REGISTRY
 echo $INPUT_DESTINATION_REGISTRY_USERNAME
 echo $INPUT_DESTINATION_REGISTRY_PASSWORD
 echo "----------------------------"
-
