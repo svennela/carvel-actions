3bc9fbc6f512607e7ed5fafbdd40deafee064f55
Sridhar Vennela <svennela@vmware.com>
2021-05-17 10:09:13 -0700

    carvel image copy

--- imgpkg-action/entrypoint.sh
+++ imgpkg-action/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -l
 
 sh -c "$INPUT_SOURCE_REGISTRY"
-echo "--------------"
+echo "----------------------------"
 echo $INPUT_SOURCE_REGISTRY
+echo $INPUT_DESTINATION_REGISTRY
+echo $INPUT_DESTINATION_REGISTRY_USERNAME
+echo $INPUT_DESTINATION_REGISTRY_PASSWORD
+echo "----------------------------"
 
